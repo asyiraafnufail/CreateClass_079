f8a6c8da7b3a6786146b0e5d176164ca4bdf3c51
asyiraafnufail <asyiraaf4@gmail.com>
2024-11-10 23:52:53 +0700

    hmm

--- 1.py
+++ 1.py
@@ -3,10 +3,13 @@ class PersegiPanjang:
         self.panjang = panjang
         self.lebar = lebar
     
-    def keliling (self):
-        return 2* (self.panjang + self.lebar)
-    
-    def luas (self):
+    def keliling (self,panjang,lebar):
+        if self.panjang >= 0 or self.lebar >= 0 :
+            2* (self.panjang + self.lebar)
+        else :
+            return
+
+    def luas (self,panjang,lebar):    
         return (self.panjang * self.lebar)
     
     def __str__ (self):
@@ -14,17 +17,17 @@ class PersegiPanjang:
 
 def main():
     try:
-        panjang = float(input("Masukkan panjang: "))
-        lebar = float(input("Masukkan lebar: "))
+        panjang = int(input("Masukkan panjang: "))
+        lebar = int(input("Masukkan lebar: "))
         
-        if panjang <= 0 or lebar <= 0:
-            print("Nilai harus diatas 0")
-            return
+        # if panjang <= 0 or lebar <= 0:
+        #     print("Nilai harus diatas 0")
+        #     return
 
         pp = PersegiPanjang(panjang, lebar)
         print(pp)
-        print("Kelilingnya: ", pp.keliling(), "cm")
-        print("Luasnya: ", pp.luas(), "cm2")
+        print("Kelilingnya: ", pp.keliling(panjang,lebar), "cm")
+        print("Luasnya: ", pp.luas(panjang,lebar), "cm2")
     
     except ValueError:
         print("Nilai harus sesuai")
